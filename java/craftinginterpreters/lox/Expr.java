b3b5f6a18ef8e7055cec50f68a9b8df5fdd797d0
ethancavanaugh <ethancavanaugh@icloud.com>
2024-02-13 20:47:11 -0500

    Adds visit and accept methods to GenerateAst tool and Expression class

--- java/craftinginterpreters/lox/Expr.java
+++ java/craftinginterpreters/lox/Expr.java
@@ -3,6 +3,15 @@ package craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Expr {
+	interface Visitor<R> {
+		R visitBinaryExpr(Binary expr);
+		R visitGroupingExpr(Grouping expr);
+		R visitLiteralExpr(Literal expr);
+		R visitUnaryExpr(Unary expr);
+	}
+
+	abstract <R> R accept(Visitor<R> visitor);
+
 	static class Binary extends Expr {
 		final Expr left;
 		final Token operator;
@@ -13,6 +22,11 @@ abstract class Expr {
 			this.operator = operator;
 			this.right = right;
 		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitBinaryExpr(this);
+		}
 	}
 	static class Grouping extends Expr {
 		final Expr expression;
@@ -20,12 +34,22 @@ abstract class Expr {
 		public Grouping(Expr expression) {
 			this.expression = expression;
 		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitGroupingExpr(this);
+		}
 	}
 	static class Literal extends Expr {
 		final Object value;
 
 		public Literal(Object value) {
 			this.value = value;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitLiteralExpr(this);
 		}
 	}
 	static class Unary extends Expr {
@@ -35,6 +59,11 @@ abstract class Expr {
 		public Unary(Token operator, Expr right) {
 			this.operator = operator;
 			this.right = right;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitUnaryExpr(this);
 		}
 	}
 }
--- java/craftinginterpreters/tools/GenerateAst.java
+++ java/craftinginterpreters/tools/GenerateAst.java
@@ -30,6 +30,13 @@ public class GenerateAst {
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+        writer.println();
+
+        writer.println("\tabstract <R> R accept(Visitor<R> visitor);");
+        writer.println();
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
@@ -42,17 +49,35 @@ public class GenerateAst {
     private static void defineType(PrintWriter writer, String baseName, String className, String fields) {
         writer.println("\tstatic class " + className + " extends " + baseName + " {");
 
+        //Fields
         for (String field : fields.split(", ")) {
             writer.println("\t\tfinal " + field + ";");
         }
         writer.println();
 
+        //Constructor
         writer.println("\t\tpublic " + className + "(" + fields + ") {");
         for (String field : fields.split(", ")) {
             String name = field.split(" ")[1];
             writer.println("\t\t\tthis." + name + " = " + name + ";");
         }
         writer.println("\t\t}");
+        writer.println();
+
+        //Accept method to call proper visitor method
+        writer.println("\t\t@Override");
+        writer.println("\t\t<R> R accept(Visitor<R> visitor) {");
+        writer.println("\t\t\treturn visitor.visit" + className + baseName + "(this);");
+        writer.println("\t\t}");
         writer.println("\t}");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("\tinterface Visitor<R> {");
+        for (String type: types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("\t\tR visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println(("\t}"));
     }
 }

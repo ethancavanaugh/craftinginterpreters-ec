d24d6772b6b683faeed1e0ed46c39c03c664b345
ethancavanaugh <ethancavanaugh@icloud.com>
2024-02-17 14:19:35 -0500

    Creates environment with define and get methods

---
+++ java/craftinginterpreters/lox/Environment.java
@@ -0,0 +1,18 @@
+package craftinginterpreters.lox;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Environment {
+    private final Map<String, Object> values = new HashMap<>();
+
+    void define(String name, Object value) {
+        values.put(name, value);
+    }
+
+    Object get(Token name) {
+        if (values.containsKey(name.lexeme)) return values.get(name.lexeme);
+
+        throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    }
+}

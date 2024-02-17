d8e7160fc732687da0a4622e7870eedf8dee83ba
ethancavanaugh <ethancavanaugh@icloud.com>
2024-02-17 11:05:31 -0500

    Adds statements to Parser

--- java/craftinginterpreters/lox/Parser.java
+++ java/craftinginterpreters/lox/Parser.java
@@ -15,13 +15,31 @@ class Parser {
         this.tokens = tokens;
     }
 
-    Expr parse() {
-        try {
-            return expression();
+    List<Stmt> parse() {
+        List<Stmt> statements = new ArrayList<>();
+        while (!atEnd()) {
+            statements.add(statement());
         }
-        catch (ParseError e) {
-            return null;
-        }
+
+        return statements;
+    }
+
+    private Stmt statement() {
+        if (match(PRINT)) return printStatement();
+
+        return expressionStatement();
+    }
+
+    private Stmt printStatement() {
+        Expr value = expression();
+        consume(SEMICOLON, "Expect ';' at end of statement.");
+        return new Stmt.Print(value);
+    }
+
+    private Stmt expressionStatement() {
+        Expr expr = expression();
+        consume(SEMICOLON, "Expect ';' at end of statement.");
+        return new Stmt.Expression(expr);
     }
 
     private Expr expression() {

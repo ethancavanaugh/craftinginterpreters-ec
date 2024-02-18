00831ed571b204c7e5b9f128926dd8ae61b2ab57
ethancavanaugh <ethancavanaugh@icloud.com>
2024-02-18 13:18:43 -0500

    Adds support for while statements to Parser and Interpreter

--- java/craftinginterpreters/lox/Parser.java
+++ java/craftinginterpreters/lox/Parser.java
@@ -48,11 +48,21 @@ class Parser {
     }
 
     private Stmt statement() {
+        if (match(WHILE)) return whileStatement();
         if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(LEFT_BRACE)) return blockStatement();
 
         return expressionStatement();
+    }
+
+    private Stmt whileStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'while'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after while condition.");
+        Stmt body = statement();
+
+        return new Stmt.While(condition, body);
     }
 
     private Stmt ifStatement() {

3797f5a379d376a1614c499699303baa19da8d81
ethancavanaugh <ethancavanaugh@icloud.com>
2024-02-18 10:18:24 -0500

    Creates Block statement class

--- java/craftinginterpreters/lox/Stmt.java
+++ java/craftinginterpreters/lox/Stmt.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 abstract class Stmt {
 	interface Visitor<R> {
+		R visitBlockStmt(Block stmt);
 		R visitExpressionStmt(Expression stmt);
 		R visitPrintStmt(Print stmt);
 		R visitVarStmt(Var stmt);
@@ -11,6 +12,18 @@ abstract class Stmt {
 
 	abstract <R> R accept(Visitor<R> visitor);
 
+	static class Block extends Stmt {
+		final List<Stmt> statements;
+
+		public Block(List<Stmt> statements) {
+			this.statements = statements;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitBlockStmt(this);
+		}
+	}
 	static class Expression extends Stmt {
 		final Expr expression;
 

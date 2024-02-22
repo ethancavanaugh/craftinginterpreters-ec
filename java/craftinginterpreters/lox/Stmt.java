815b2901108de5b55e69cb991545e68e0cd9c09c
ethancavanaugh <ethancavanaugh@icloud.com>
2024-02-22 16:37:13 -0500

    Creates return statement class

--- java/craftinginterpreters/lox/Stmt.java
+++ java/craftinginterpreters/lox/Stmt.java
@@ -11,6 +11,7 @@ abstract class Stmt {
 		R visitFunctionStmt(Function stmt);
 		R visitIfStmt(If stmt);
 		R visitPrintStmt(Print stmt);
+		R visitReturnStmt(Return stmt);
 		R visitVarStmt(Var stmt);
 		R visitWhileStmt(While stmt);
 	}
@@ -103,6 +104,20 @@ abstract class Stmt {
 		@Override
 		<R> R accept(Visitor<R> visitor) {
 			return visitor.visitPrintStmt(this);
+		}
+	}
+	static class Return extends Stmt {
+		final Token keyword;
+		final Expr value;
+
+		public Return(Token keyword, Expr value) {
+			this.keyword = keyword;
+			this.value = value;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitReturnStmt(this);
 		}
 	}
 	static class Var extends Stmt {

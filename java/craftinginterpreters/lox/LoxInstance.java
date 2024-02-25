3a7081149d3687486d2e604caef8d4a01da6c05c
ethancavanaugh <ethancavanaugh@icloud.com>
2024-02-25 10:34:02 -0500

    Supports basic object instantiation

---
+++ java/craftinginterpreters/lox/LoxInstance.java
@@ -0,0 +1,17 @@
+package craftinginterpreters.lox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoxInstance {
+    private LoxClass klass;
+
+    LoxInstance(LoxClass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String toString() {
+        return klass.name + " instance";
+    }
+}
